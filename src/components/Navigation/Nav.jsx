94fd74f0a3beae8b3331c3023ceb154ead368225
Prit Manvar <pritmanvar1109@gmail.com>
2023-01-23 21:05:59 +0530

    minor change in navigation bar

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "./Logo";
 
 const Nav = ({ selectedIteam }) => {
+    console.log(selectedIteam, typeof selectedIteam);
     return (
         <div className='w-1/5 bg-nav-bg h-full fixed text-main-font font-semibold lg:text-lg md:text-sm text-xs'>
             <Logo />
@@ -10,65 +11,145 @@ const Nav = ({ selectedIteam }) => {
             {/* Give class names conditonally using selectedIteam for greadient hover effect */}
             <div
                 className={`hover:bg-gradient-to-r from-my-yellow-gradient to-transparent hover:border-my-yellow navIteam ${
-                    selectedIteam == "statistics" ? "navStatistics" : ""
+                    selectedIteam === "statistics" ? "navStatistics" : ""
                 }`}>
-                <img
-                    className='mx-2 inline-block'
-                    src='https://img.icons8.com/ios-filled/20/828282/combo-chart--v1.png'
-                />
-                <p className='inline-block text-secondary-font'>Statistics</p>
+                {selectedIteam === "statistics" ? (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/ios-filled/20/ededed/combo-chart--v1.png'
+                    />
+                ) : (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/ios-filled/20/828282/combo-chart--v1.png'
+                    />
+                )}
+                <p
+                    className={`inline-block ${
+                        selectedIteam === "statistics"
+                            ? "text-main-font"
+                            : "text-secondary-font"
+                    }`}>
+                    Statistics
+                </p>
             </div>
             <div
                 className={`hover:bg-gradient-to-r from-my-green-gradient to-transparent hover:border-my-green navIteam ${
-                    selectedIteam == "yourgroups" ? "navYourGroups" : ""
+                    selectedIteam === "yourgroups" ? "navYourGroups" : ""
                 }`}>
-                <img
-                    className='mx-2 inline-block'
-                    src='https://img.icons8.com/fluency-systems-filled/22/828282/groups.png'
-                />
-                <p className='inline-block text-secondary-font'>Your Groups</p>
+                {selectedIteam === "yourgroups" ? (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/fluency-systems-filled/22/ededed/groups.png'
+                    />
+                ) : (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/fluency-systems-filled/22/828282/groups.png'
+                    />
+                )}
+                <p
+                    className={`inline-block ${
+                        selectedIteam === "yourgroups"
+                            ? "text-main-font"
+                            : "text-secondary-font"
+                    }`}>
+                    Your Groups
+                </p>
             </div>
             <div
                 className={`hover:bg-gradient-to-r from-my-purple-gradient to-transparent hover:border-my-purple navIteam ${
-                    selectedIteam == "submissions" ? "navSubmissions" : ""
+                    selectedIteam === "submissions" ? "navSubmissions" : ""
                 }`}>
-                <img
-                    className='mx-2 inline-block'
-                    src='https://img.icons8.com/ios-filled/22/828282/source-code.png'
-                />
-                <p className='inline-block text-secondary-font'>Submissions</p>
+                {selectedIteam === "submissions" ? (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/ios-filled/22/ededed/source-code.png'
+                    />
+                ) : (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/ios-filled/22/828282/source-code.png'
+                    />
+                )}
+                <p
+                    className={`inline-block ${
+                        selectedIteam === "submissions"
+                            ? "text-main-font"
+                            : "text-secondary-font"
+                    }`}>
+                    Submissions
+                </p>
             </div>
             <div
                 className={`hover:bg-gradient-to-r from-my-light-blue-gradient to-transparent hover:border-my-light-blue navIteam ${
-                    selectedIteam == "problemset" ? "navProblemSet" : ""
+                    selectedIteam === "problemset" ? "navProblemSet" : ""
                 }`}>
-                <img
-                    className='mx-2 inline-block'
-                    src='https://img.icons8.com/material-outlined/22/828282/puzzle.png'
-                />
-                <p className='inline-block text-secondary-font'>Problem Set</p>
+                {selectedIteam === "problemset" ? (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/material-outlined/22/ededed/puzzle.png'
+                    />
+                ) : (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/material-outlined/22/828282/puzzle.png'
+                    />
+                )}
+                <p
+                    className={`inline-block ${
+                        selectedIteam === "problemset"
+                            ? "text-main-font"
+                            : "text-secondary-font"
+                    }`}>
+                    Problem Set
+                </p>
             </div>
             <div
                 className={`hover:bg-gradient-to-r from-my-pink-gradient to-transparent hover:border-my-pink navIteam ${
-                    selectedIteam == "yourproblems" ? "navYourProblems" : ""
+                    selectedIteam === "yourproblems" ? "navYourProblems" : ""
                 }`}>
-                <img
-                    className='mx-2 inline-block'
-                    src='https://img.icons8.com/material-outlined/22/828282/christmas-star.png'
-                />
-                <p className='inline-block text-secondary-font'>
+                {selectedIteam === "yourproblems" ? (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/material-outlined/22/ededed/christmas-star.png'
+                    />
+                ) : (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/material-outlined/22/828282/christmas-star.png'
+                    />
+                )}
+                <p
+                    className={`inline-block ${
+                        selectedIteam === "yourproblems"
+                            ? "text-main-font"
+                            : "text-secondary-font"
+                    }`}>
                     Your Problems
                 </p>
             </div>
             <div
                 className={`hover:bg-gradient-to-r from-my-orange-gradient to-transparent hover:border-my-orange navIteam ${
-                    selectedIteam == "compareusers" ? "navCompareUsers" : ""
+                    selectedIteam === "compareusers" ? "navCompareUsers" : ""
                 }`}>
-                <img
-                    className='mx-2 inline-block'
-                    src='https://img.icons8.com/ios-glyphs/22/828282/compare.png'
-                />
-                <p className='inline-block text-secondary-font'>
+                {selectedIteam === "compareusers" ? (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/ios-glyphs/22/ededed/compare.png'
+                    />
+                ) : (
+                    <img
+                        className='mx-2 inline-block'
+                        src='https://img.icons8.com/ios-glyphs/22/828282/compare.png'
+                    />
+                )}
+                <p
+                    className={`inline-block ${
+                        selectedIteam === "compareusers"
+                            ? "text-main-font"
+                            : "text-secondary-font"
+                    }`}>
                     Compare Users
                 </p>
             </div>
